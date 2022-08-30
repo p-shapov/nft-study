a2fb4b814d2d1ff34db4a592cf6ac98a11bb1b5e
Pavel Shapov <pavel.shapov@metalamp.io>
2022-08-30 07:38:48 +0700

    Fix overflow issues in layout. Add lazy loading for image

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,7 +1,8 @@
 // TODO :: Add lazy loading
 
 import cn from 'classnames';
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import styles from './module.scss';
 
@@ -14,16 +15,29 @@ export type Props = {
 
 export const Image: FC<Props> = ({ src, alt, timeout, lazy }) => {
   const [loaded, setLoaded] = useState(false);
+  const timeoutId = useRef<ReturnType<typeof setTimeout>>();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: '200px 0px',
+    skip: !lazy,
+  });
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId.current) clearTimeout(timeoutId.current);
+    };
+  }, []);
 
   const handleLoad = () => {
-    if (timeout) setTimeout(() => setLoaded(true), timeout);
+    if (timeout) timeoutId.current = setTimeout(() => setLoaded(true), timeout);
     else setLoaded(true);
   };
 
   return (
     <img
+      ref={ref}
       className={cn(styles['image'], loaded && styles['image--loaded'])}
-      src={src}
+      src={inView || !lazy ? src : undefined}
       alt={alt}
       onLoad={handleLoad}
     />
